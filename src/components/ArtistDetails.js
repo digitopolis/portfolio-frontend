f48bf83ad5ec029511e0ae2fbafaac6e7b58e46a
Matt Readout <matt.readout@gmail.com>
2019-02-04 14:46:47 -0500

    artist profile page

--- src/components/ArtistDetails.js
+++ src/components/ArtistDetails.js
@@ -1,14 +1,31 @@
 import React from 'react'
 import Work from './Work'
 import { connect } from 'react-redux'
-import { Grid } from 'semantic-ui-react'
+import { Grid, Image, Header, List } from 'semantic-ui-react'
 
 class ArtistDetails extends React.Component {
 	render () {
 		return (
 			<Grid celled='internally'>
-				<Grid.Row textAlign='center'>
-					<h1>{this.props.artist.name}</h1>
+				<Grid.Row>
+					<Grid.Column textAlign='center'>
+						<Header size='large'>{this.props.artist.name}</Header>
+						<Image
+							centered
+							rounded
+							size='medium'
+							src={this.props.artist.img_url}
+						/>
+						<List horizontal divided>
+							<List.Item>
+								<List.Content>{this.props.artist.location}</List.Content>
+							</List.Item>
+							<List.Item>
+								<List.Content>{this.props.artist.media}</List.Content>
+							</List.Item>
+						</List>
+						<Header size='medium'>{this.props.artist.bio}</Header>
+					</Grid.Column>
 				</Grid.Row>
 				<Grid.Row>
 					{this.props.artist.works.map(work => {
