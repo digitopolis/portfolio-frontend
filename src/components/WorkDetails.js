e789758261965cfc7e4e37ee2229a6e168098d49
Matt Readout <matt.readout@gmail.com>
2019-02-04 14:01:38 -0500

    work details page

--- src/components/WorkDetails.js
+++ src/components/WorkDetails.js
@@ -1,23 +1,55 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Icon, Button } from 'semantic-ui-react'
+import { Grid, Icon, Button, Image, List } from 'semantic-ui-react'
 
 class WorkDetails extends React.Component {
 	render () {
 		return (
-			<Button
-				icon
-				onClick={this.props.deselectWork}>
-				<Icon name='close' />
-			</Button>
+			<Grid>
+				<Grid.Row>
+					<Button
+						icon
+						onClick={this.props.deselectWork}>
+						<Icon name='close' />
+					</Button>
+				</Grid.Row>
+				<Grid.Row>
+					<Grid.Column width={10}>
+						<Image src={this.props.work.img_url}/>
+					</Grid.Column>
+					<Grid.Column width={4}>
+						<List>
+							<List.Item>
+								<List.Header>{this.props.work.title}</List.Header>
+							</List.Item>
+							<List.Item>
+								<List.Icon name='user' />
+								<List.Content>{this.props.artist.name}</List.Content>
+							</List.Item>
+							<List.Item>
+								<List.Icon name='calendar alternate outline' />
+								<List.Content>{this.props.work.year}</List.Content>
+							</List.Item>
+							<List.Item>
+								<List.Icon name='paint brush' />
+								<List.Content>{this.props.work.media}</List.Content>
+							</List.Item>
+							<List.Item>
+								<List.Content>{this.props.work.statement}</List.Content>
+							</List.Item>
+						</List>
+					</Grid.Column>
+				</Grid.Row>
+			</Grid>
+
 		)
 	}
 }
 
 const mapStateToProps = (state) => {
-	return {
-		work: state.works.find(w => w.id === state.selectedWork)
-	}
+	const work = state.works.find(w => w.id === state.selectedWork)
+	const artist = state.artists.find(a => a.id === work.artist_id)
+	return { work, artist }
 }
 
 const mapDispatchToProps = (dispatch) => {
