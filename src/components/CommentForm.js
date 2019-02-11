c163d5ac8eaa7eca10acbc1d832dfddabeb66588
Matt Readout <matt.readout@gmail.com>
2019-02-11 18:00:36 -0500

    post new comment

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,12 +1,63 @@
 import React from 'react'
+import { COMMENTS } from '../apiEndpoints'
+import { Segment, Form } from 'semantic-ui-react'
+import { connect } from 'react-redux'
 
 
 class CommentForm extends React.Component {
+
+	state = {
+		content: ''
+	}
+
+	handleChange = (event) => {
+		const content = event.target.value
+		this.setState({ content })
+	}
+
+	handleSubmit = () => {
+		const jwt = localStorage.getItem('jwt')
+		fetch(COMMENTS, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				'Accept': 'application/json',
+				'Authorization': `Bearer ${jwt}`
+			},
+			body: JSON.stringify({
+				content: this.state.content,
+				artist_id: this.props.artist,
+				work_id: this.props.work
+			})
+		}).then(res => res.json())
+		.then(this.handleResponse)
+	}
+
+	handleResponse = (res) => {
+		console.log(res);
+		//find the work, add the new comment to the comments, then dispatch updated work to reducer to update store
+	}
+
 	render () {
 		return (
-			<h1>new comment form</h1>
+			<Segment>
+				<Form onSubmit={this.handleSubmit}>
+					<Form.TextArea
+						label='Comment'
+						onChange={this.handleChange}
+					/>
+					<Form.Button>Post</Form.Button>
+				</Form>
+			</Segment>
 		)
 	}
 }
 
-export default CommentForm;
+const mapStateToProps = (state) => {
+	return {
+		artist: state.currentUser.id,
+		work: state.selectedWork
+	}
+}
+
+export default connect(mapStateToProps)(CommentForm);
